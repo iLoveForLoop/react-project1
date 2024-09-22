fe737c9f806348edb03af943245634e7b1d6c7fa
iLoveForLoop <jefbay110@gmail.com>
2024-09-22 20:41:40 +0800

    re-work

--- src/App.jsx
+++ src/App.jsx
@@ -8,169 +8,204 @@ import CustomButton from "./components/button/button";
 import "./App.css";
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-  const [txtName, setTxtName] = useState("");
-  const [textPrice, setTextPrice] = useState("");
-  const [textQuantity, setTextQuantity] = useState("");
-  const [editingIndex, setEditingIndex] = useState(null);
-  const [total, setTotal] = (useState(0));
+	const [cartItems, setCartItems] = useState([]);
+	const [txtName, setTxtName] = useState("");
+	const [textPrice, setTextPrice] = useState("");
+	const [textQuantity, setTextQuantity] = useState("");
+	const [editingIndex, setEditingIndex] = useState(null);
+	const [shippingFee, setShippingFee] = useState(0);
+	let subTotal = 0;
 
-  function onChange(e) {
-    const id = e.target.id;
-    const value = e.target.value;
+	const towns = ["tubigon", "calape", "loon"];
+	const fee = {
+		tubigon: 50,
+		calape: 100,
+		loon: 150,
+	};
 
-    if (id === "txtName") setTxtName(value);
-    if (id === "txtPrice") setTextPrice(value);
-    if (id === "txtQuantity") setTextQuantity(value);
-  }
+	function onChange(e) {
+		const id = e.target.id;
+		const value = e.target.value;
 
-  function addToChart() {
-    if ((txtName, textPrice, textQuantity)) {
-      const item = {
-        name: txtName,
-        price: textPrice,
-        quantity: textQuantity,
-      };
+		if (id === "txtName") setTxtName(value);
+		if (id === "txtPrice") setTextPrice(value);
+		if (id === "txtQuantity") setTextQuantity(value);
+		if (towns.includes(value)) {
+			setShippingFee(fee[value]);
+		}
+	}
 
-      if(editingIndex !== null){
-        const updatedItems = [...cartItems];
-        updatedItems[editingIndex] = item;
-        setCartItems(updatedItems);
-        setEditingIndex(null);
+	function formatCurrency(num) {
+		const formatter = new Intl.NumberFormat("en-US", {
+			style: "currency",
+			currency: "PHP",
+			minimumFractionDigits: 2,
+			maximumFractionDigits: 2,
+		});
+		return formatter.format(num);
+	}
 
-      }else{
-        const newItems = [...cartItems, item];
-        setCartItems(newItems);
-      }
-      
-      const total = item.price * item.quantity;
-      setTotal((data) => data += total )
-      setTxtName("");
-      setTextPrice("");
-      setTextQuantity("");
-    }
-  }
+	function addToChart() {
+		if ((txtName, textPrice, textQuantity)) {
+			const item = {
+				name: txtName,
+				price: textPrice,
+				quantity: textQuantity,
+			};
 
-  function deleteItem(itemIndex) {
-    console.log(itemIndex);
-    const total = cartItems[itemIndex].price * cartItems[itemIndex].quantity;
-    setTotal((data) => data -= total );
-    const updatedItems = [...cartItems];
-    updatedItems.splice(itemIndex, 1);
+			if (editingIndex !== null) {
+				const updatedItems = [...cartItems];
+				updatedItems[editingIndex] = item;
+				setCartItems(updatedItems);
+				setEditingIndex(null);
+			} else {
+				const newItems = [...cartItems, item];
+				setCartItems(newItems);
+			}
 
+			setTxtName("");
+			setTextPrice("");
+			setTextQuantity("");
+		}
+	}
 
-    setCartItems(updatedItems);
-    
-  }
+	function deleteItem(itemIndex) {
+		const updatedItems = [...cartItems];
+		updatedItems.splice(itemIndex, 1);
+		setCartItems(updatedItems);
+	}
 
-  function editItem(index){
-    setEditingIndex(index)
-    setTxtName(cartItems[index].name);
-    setTextPrice(cartItems[index].price);
-    setTextQuantity(cartItems[index].quantity);
-  }
+	function editItem(index) {
+		setEditingIndex(index);
+		setTxtName(cartItems[index].name);
+		setTextPrice(cartItems[index].price);
+		setTextQuantity(cartItems[index].quantity);
+	}
 
-  return (
-    <div>
-      <div className="main-container">
-        <div className="sub-container">
-          <Textbox
-            id="txtName"
-            type="text"
-            label="item name"
-            value={txtName}
-            containerClass="p-3"
-            onTextChange={onChange}
-          />
-          <Textbox
-            id="txtPrice"
-            type="number"
-            label="item price"
-            value={textPrice}
-            containerClass="p-3"
-            onTextChange={onChange}
-          />
-          <Textbox
-            id="txtQuantity"
-            type="number"
-            label="quantity"
-            value={textQuantity}
-            containerClass="p-3"
-            onTextChange={onChange}
-          />
-          <Dropdown
-            id="drpTown"
-            label="town"
-            options={["tubigon", "calape"]}
-            containerClass="p-3"
-            onSelectChange={onChange}
-          />
-          <Dropdown
-            name="drpPayment"
-            label="payment method"
-            options={["gcash", "creditcard"]}
-            containerClass="p-3"
-            onSelectChange={onChange}
-          />
-          <div className="d-flex justify-content-center py-2">
-            <CustomButton
-              label="add to cart"
-              onClick={addToChart}
-              variant="primary"
-            >{editingIndex !== null ? 'UPDATE' : 'ADD TO CART'}</CustomButton>
-          </div>
-        </div>
-        {cartItems.length > 0 && (
-          <div className="item-container my-5">
-            <h3 className="text-center py-3">CART ITEMS</h3>
-            <Table striped bordered>
-              <thead>
-                <tr className="text-capitalize">
-                  <th>item#</th>
-                  <th>item name</th>
-                  <th>price</th>
-                  <th>quantity</th>
-                  <th>total</th>
-                  <td>actions</td>
-                </tr>
-              </thead>
-              <tbody>
-                {cartItems.map((item, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{index + 1}</td>
-                      <td>{item.name}</td>
-                      <td>{item.price}</td>
-                      <td>{item.quantity}</td>
-                      <td>{item.price * item.quantity}</td>
-                      <td className="text-center" width={200}>
-                        <CustomButton
-                          label="edit"
-                          variant="success"
-                          innerClass="m-1"
-                          onClick={() => editItem(index)}
-                        />
-                        <CustomButton
-                          label="delete"
-                          variant="danger"
-                          innerClass="m-1"
-                          onClick={() => deleteItem(index)}
-                        />
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-              <tfoot>
-                <th>Total</th>
-                <td>${total}</td>
-              </tfoot>
-            </Table>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	function clearCart() {
+		setCartItems([]);
+	}
+
+	return (
+		<div>
+			<div className="main-container">
+				<div className="sub-container">
+					<h3 className="text-center py-3 text-bg-dark">ITEMS</h3>
+					<Textbox
+						id="txtName"
+						type="text"
+						label="item name"
+						value={txtName}
+						containerClass="p-3"
+						onTextChange={onChange}
+					/>
+					<Textbox
+						id="txtPrice"
+						type="number"
+						label="item price"
+						value={textPrice}
+						containerClass="p-3"
+						onTextChange={onChange}
+					/>
+					<Textbox
+						id="txtQuantity"
+						type="number"
+						label="quantity"
+						value={textQuantity}
+						containerClass="p-3"
+						onTextChange={onChange}
+					/>
+					<div className="d-flex justify-content-center py-2">
+						<CustomButton
+							label={editingIndex !== null ? "Update" : "Add To Cart"}
+							onClick={addToChart}
+							variant="primary"
+						/>
+					</div>
+				</div>
+				{cartItems.length > 0 && (
+					<div className="item-container my-5">
+						<h3 className="text-center py-3 text-bg-dark">CART ITEMS</h3>
+						<Table striped bordered>
+							<thead>
+								<tr className="text-capitalize">
+									<th>item#</th>
+									<th>item name</th>
+									<th>price</th>
+									<th>quantity</th>
+									<th>total</th>
+									<td>actions</td>
+								</tr>
+							</thead>
+							<tbody>
+								{cartItems.map((item, index) => {
+									subTotal += item.price * item.quantity;
+
+									return (
+										<tr key={index}>
+											<td>{index + 1}</td>
+											<td>{item.name}</td>
+											<td>{formatCurrency(item.price)}</td>
+											<td>{item.quantity}</td>
+											<td>{formatCurrency(item.price * item.quantity)}</td>
+											<td className="text-center" width={200}>
+												<CustomButton
+													label="edit"
+													variant="success"
+													innerClass="m-1"
+													onClick={() => editItem(index)}
+												/>
+												<CustomButton
+													label="delete"
+													variant="danger"
+													innerClass="m-1"
+													onClick={() => deleteItem(index)}
+												/>
+											</td>
+										</tr>
+									);
+								})}
+							</tbody>
+						</Table>
+						<div className="d-flex justify-content-between">
+							<div className="d-flex">
+								<Dropdown
+									id="drpTown"
+									label="town"
+									options={towns}
+									containerClass="d-flex p-3"
+									onSelectChange={onChange}
+								/>
+								<Dropdown
+									name="drpPayment"
+									label="payment method"
+									options={["gcash", "creditcard"]}
+									containerClass="d-flex py-3"
+									onSelectChange={onChange}
+								/>
+							</div>
+
+							<div className="d-flex justify-content-center align-items-center">
+								<CustomButton
+									label="clear"
+									variant="danger"
+									innerClass="my-1 me-2"
+									onClick={clearCart}
+								/>
+							</div>
+						</div>
+						<div className="ms-2">
+							<p className="mb-0 fs-6">SubTotal: {formatCurrency(subTotal)}</p>
+							<p className="mb-0 fs-6">
+								Shipping Fee: {formatCurrency(shippingFee)}
+							</p>
+							<h5>GrandTotal: {formatCurrency(subTotal + shippingFee)}</h5>
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -2,22 +2,23 @@
 import Form from "react-bootstrap/Form";
 
 function Dropdown(props) {
-  return (
-    <div className={props.containerClass}>
-      <Form.Select
-        className="text-capitalize"
-        size="lg"
-        onChange={props.onSelectChange}
-      >
-        <option value="">{`select ${props.label}`}</option>
-        {props.options.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-      </Form.Select>
-    </div>
-  );
+	return (
+		<div className={props.containerClass}>
+			<Form.Select
+				id="select"
+				className="text-capitalize"
+				size="lg"
+				onChange={props.onSelectChange}
+			>
+				<option value="">{`select ${props.label}`}</option>
+				{props.options.map((option, index) => (
+					<option key={index} value={option}>
+						{option}
+					</option>
+				))}
+			</Form.Select>
+		</div>
+	);
 }
 
 export default Dropdown;
